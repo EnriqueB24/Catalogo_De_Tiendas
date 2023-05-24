d88d0d0a238a8f1eb25fde39df7a95d72f6a64e4
EnriqueB24 <luis.bernal5293@alumnos.udg.mx>
2023-05-24 00:13:49 -0600

    Initial commit

---
+++ app/src/main/java/app/ejemplo/CatdeTiendas/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.ejemplo.CatdeTiendas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.catalogodetiendas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/app/ejemplo/CatdeTiendas/Tienda.kt
@@ -0,0 +1,9 @@
+package app.ejemplo.CatdeTiendas
+
+import java.util.UUID
+
+data class Tienda(
+    val id: UUID,
+    val nombre:String,
+    val telefono:String
+)
